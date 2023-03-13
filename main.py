3616b7a6321cf639beb9e0ec0a280b9395bf8ca5
Kris Warner <kdwarn@protonmail.com>
2023-03-13 20:35:56 +0000

    Add endpoint for record for report

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from typing import Any, Optional
+import datetime
+
 import oracledb
+
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from config import USER, PASSWORD
 
@@ -13,6 +17,52 @@ oracledb.defaults.config_dir = "."
 
 class Error(BaseModel):
     message: str
+
+
+class ReportRecord(BaseModel):
+    RECORDNUM: int = Field(alias="record_num")
+    TYPE: Optional[str] = Field(alias="type")
+    SETDATE: Optional[datetime.date] = Field(alias="date")  # maybe date
+    TAKENBY: Optional[str] = Field(alias="taken_by")
+    COUNTERID: Optional[str] = Field(alias="counter_id")
+    STATIONID: Optional[str] = Field(alias="station_id")
+    DESCRIPTION: Optional[str] = Field(alias="description")
+    PRJ: Optional[str] = Field(alias="project")
+    PROGRAM: Optional[str] = Field(alias="program")
+    BIKEPEDGROUP: Optional[str] = Field(alias="group")
+    BIKEPEDFACILITY: Optional[str] = Field(alias="facility")
+    SR: Optional[str]
+    SEQ: Optional[str]
+    OFFSET: Optional[str] = Field(alias="offset")
+    SRI: Optional[str]
+    MP: Optional[str]
+    LATITUDE: Optional[float] = Field(alias="latitude")
+    LONGITUDE: Optional[float] = Field(alias="longitude")
+    MCD: Optional[str]
+    ROUTE: Optional[int] = Field(alias="route")
+    ROAD: Optional[str] = Field(alias="road")
+    RDPREFIX: Optional[str] = Field(alias="road_prefix")
+    RDSUFFIX: Optional[str] = Field(alias="road_suffix")
+    ISURBAN: Optional[str] = Field(alias="is_urban")
+    SIDEWALK: Optional[str] = Field(alias="sidewalk")
+    OUTDIR: Optional[str] = Field(alias="out_direction")
+    INDIR: Optional[str] = Field(alias="in_direction")
+    CNTDIR: Optional[str] = Field(alias="counter_direction")
+    TRAFDIR: Optional[str] = Field(alias="traffic_direction")
+    FC: Optional[int]
+    SPEEDLIMIT: Optional[int] = Field(alias="speed_limit")
+    WEATHER: Optional[str] = Field(alias="weather")
+    # county: Optional[str] = Field(alias="")
+    # count_data: List[ReportCountData]
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class ReportCountData(BaseModel):
+    date: str
+    time: str
+    count: int
 
 
 # test3
@@ -62,3 +112,25 @@ def get_record_nums():
         records.append(row[0])
 
     return records
+
+
+@app.get(
+    "/api/traffic-counts/v1/report/record/{num}",
+    responses=responses,
+    response_model=ReportRecord,
+)
+def get_record(num: int) -> Any:
+    with oracledb.connect(user=USER, password=PASSWORD, dsn="dvrpcprod_tp_tls") as connection:
+        with connection.cursor() as cursor:
+            cursor = connection.cursor()
+            cursor.execute("select * from DVRPCTC.TC_HEADER where RECORDNUM = :num", num=num)
+
+            # convert tuple to dictionary
+            # <https://python-oracledb.readthedocs.io/en/latest/user_guide/sql_execution.html#rowfactories>
+            columns = [col[0] for col in cursor.description]
+            cursor.rowfactory = lambda *args: dict(zip(columns, args))
+            data = cursor.fetchone()
+
+    if data is None:
+        return JSONResponse(status_code=404, content={"message": "Record not found"})
+    return data
