d0e0df786ae6d7578d0c294787d3ad548e91194a
Kris Warner <kdwarn@protonmail.com>
2025-05-06 20:01:37 +0000

    Fix volume counts & query params in records/.

--- metadata.py
+++ metadata.py
@@ -15,6 +15,7 @@ from counts import (
     NotInDatabaseCountKind,
     PedestrianCountKind,
     VehicleCountKind,
+    AllCountKinds,
 )
 
 logger = logging.getLogger("api")
@@ -31,7 +32,9 @@ class Metadata(BaseModel):
     RECORDNUM: int = Field(alias="record_num")
     SOURCE: Optional[str]
     count_type: Optional[CountKind] = None
-    sub_type: Optional[VehicleCountKind] = None
+    sub_type: Optional[
+        Union[BicycleCountKind, PedestrianCountKind, VehicleCountKind, NotInDatabaseCountKind]
+    ] = None
     AADV: Optional[int]
     AM_PEAK_VOLUME: Optional[int]
     AVG_AM_MAX_PERCENT: Optional[float]
@@ -93,9 +96,7 @@ router = APIRouter()
 )
 def get_count_numbers(
     count_type: Optional[CountKind] = None,
-    sub_type: Optional[
-        Union[BicycleCountKind, PedestrianCountKind, VehicleCountKind, NotInDatabaseCountKind]
-    ] = None,
+    sub_type: Optional[AllCountKinds] = None,
 ) -> list[int]:
     """
     Get the record numbers of all counts, in descending order.
